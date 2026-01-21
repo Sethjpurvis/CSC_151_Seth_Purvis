fb0ac69a5824eeae5da7a61bd1628c493af58735
SANDHILLS\LH218-22 <jad.talbert@gmail.com>
2026-01-21 11:37:12 -0500

    my first Java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,15 @@
+/* 
+@author: Seth Purvis
+@date: 01/21/2026
+@purpose: my first Java program
+*/
 
+
+// This is the class name
+public class HelloWorld
+{ 
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
