c0f3a1ee426abf032e5ba29a15d8f6b5b04fbd51
SANDHILLS\LH218-22 <jad.talbert@gmail.com>
2026-02-25 11:09:46 -0500

    2/25

---
+++ src/labs/example/loops/WhileLoop.java
@@ -0,0 +1,19 @@
+package labs.example.loops;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        executeWhileLoop();
+    }
+
+    private static void executeWhileLoop(){
+        int counter = 0;
+        while (counter < 10) {
+            counter++;
+        }
+    }
+
+    System.out.println("The loop executed " + counter + " times");
+    return;
+
+}
+
